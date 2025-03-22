2678c140f6690fcb66df6c50c8a4d6fa92b4c455
CarlosRodrigoSchimanski <carlao64@gmail.com>
2025-03-22 13:40:52 -0300

    add props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react'
 import Tasks from './components/Tasks'
 function App() {
+  const [tasks,setTasks] = useState([
+    {
+      id:1,
+      title:'Estudar programação',
+      descripition: 'Estudar programação para se tornar um desenvolvedor',
+      status: false
+    },
+    {
+      id:2,
+      title:'Tomar café',
+      descripition: 'Tomar um café para relaxar',
+      status: false
+    },
+    {
+      id:3,
+      title:'Estudar JavaScript',
+      descripition: 'Estudar JavaScript para aprender a programar',
+      status: false
+    }
+  ])
   return(
-    <div>
-      <h1 className='text-red-500'>Gerenciador de tarefas </h1>
-      <Tasks/>
+    <div className='w-screen h-screen bg-slate-800 flex justify-center p-6'>
+      <div className='w-[500px]'>
+        <h1 className='text-center text-3xl font-bold text-lime-300'>Gerenciador de tarefas </h1>
+        <Tasks tasks={tasks}/>
+      </div>
+      
     </div>
   )
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,14 @@
-function Tasks () {
+function Tasks (props) {
+    //pasar o titulo das tarefaz
     return (
-        <h1>ola</h1>
+        <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+            {props.tasks.map((task) => (
+                <li key={task.id} className="flex space-x-2">
+                    <button className="w-full bg-slate-800 text-lime-300 p-2 rounded-md">{task.title}</button>
+                    <button className="bg-slate-800 text-lime-300 p-2 rounded-md">ver detalhes</button>
+                </li>
+            ))}
+        </ul>
     )
 }
 
